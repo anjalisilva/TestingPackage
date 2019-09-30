5dce7ba5338bfab273b9fd2cf13a55865597890f
anjalisilva <anjali@uoguelph.ca>
2019-09-29 22:00:49 -0400

    updated test functions

--- tests/testthat/test-InfCriteria_calculation.R
+++ tests/testthat/test-InfCriteria_calculation.R
@@ -1,7 +1,7 @@
-test_that("model fitting", {
-  library(TestingPackage)
+context("InfCriteria calculation")
+library(TestingPackage)
 
-  set.seed(1234)
+test_that("model fitting with three clusters", {
 
   loglikelihood = -1001.01
   clusters = 3
@@ -22,5 +22,24 @@ test_that("model fitting", {
 })
 
 
+test_that("model fitting with two clusters", {
+  
+  loglikelihood = -2001.01
+  clusters = 2
+  dimension = 6
+  observations = 1000
+  probability = c(0.4, 0.6)
+  
+  Information_criteria <- InfCriteria_v3(loglikelihood = loglikelihood,
+    clusters = clusters,
+    dimension = dimension,
+    observations = observations,
+    probability = probability)
+  
+  expect_that(Information_criteria, is_a("InfCriteria"))
+  expect_that(trunc(Information_criteria$BICresults), equals(4195))
+  expect_that(trunc(Information_criteria$AICresults), equals(4058))
+  expect_that(trunc(Information_criteria$ICLresults), equals(4195))
+})
 
 
