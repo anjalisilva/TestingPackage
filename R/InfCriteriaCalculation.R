e6de40149741687d348aea4a5b82856d2c1f7af6
Anjali Silva <anjali.silva@uhnresearch.ca>
2020-10-26 21:31:30 -0400

    Update InfCriteriaCalculation.R

--- R/InfCriteriaCalculation.R
+++ R/InfCriteriaCalculation.R
@@ -6,8 +6,8 @@
 #'
 #' @param loglikelihood A negative value of class "numeric" indicating
 #'    the log-likelihood
-#' @param clusters A positive integer indicating the number of clusters
-#' @param dimension A positive integer indicating the dimension of dataset
+#' @param nClusters A positive integer indicating the number of clusters
+#' @param dimensionality A positive integer indicating the dimensionality of dataset
 #' @param observations A positive integer indicating the number of observations
 #' @param probability A vector indicating the probability of each cluster
 #'
@@ -19,13 +19,31 @@
 #' }
 #'
 #' @examples
+#' # Using GeneCounts dataset available with package
+#' dim(GeneCounts)
+#'
+#' # Calculate information criteria value
 #' InfCriteriaResults <- InfCriteriaCalculation(loglikelihood = -5080,
-#'                                              clusters = 2,
-#'                                              dimension = 3,
-#'                                              observations = 1000,
+#'                                              nClusters = 2,
+#'                                              dimensionality = ncol(GeneCounts),
+#'                                              observations = nrow(GeneCounts),
 #'                                              probability = c(0.5, 0.5))
 #' InfCriteriaResults$BICresults
 #'
+#' \dontrun{
+#' # Obtain an external sample RNAseq dataset
+#' library(MBCluster.Seq)
+#' data("Count")
+#' dim(Count)
+#'
+#' # Calculate information criteria value
+#' InfCriteriaResults <- InfCriteriaCalculation(loglikelihood = -5080,
+#'                                              nClusters = 2,
+#'                                              dimensionality = ncol(Count),
+#'                                              observations = nrow(Count),
+#'                                              probability = c(0.5, 0.5))
+#' InfCriteriaResults$BICresults
+#'}
 #' @references
 #'Akaike, H. (1973). Information theory and an extension of the maximum
 #'likelihood principle. In \emph{Second International Symposium on Information
@@ -41,34 +59,66 @@
 #' @export
 #' @import mclust
 #' @import stats
-InfCriteriaCalculation <- function(loglikelihood, clusters, dimension,
-  observations, probability) {
+InfCriteriaCalculation <- function(loglikelihood,
+                                   nClusters,
+                                   dimensionality,
+                                   observations,
+                                   probability) {
+  
+  # Performing checks of user input
+  if (typeof(loglikelihood) != "double" & class(loglikelihood) != "numeric") {
+    stop("Loglikelihood type needs to double")
+  }
+  
+  if(loglikelihood > 0) {
+    stop("Loglikelihood should be a negative value.")
+  }
+  
+  if(nClusters <= 0) {
+    stop("nClusters should be a positive integer indicating the number of clusters.")
+  }
+  
+  # An alternative to correct user input
+  if(nClusters < 0) {
+    warning("nClusters should be a positive integer indicating the number of clusters.
+            Input value corrected to be positive.", call. = FALSE)
+    nClusters <- abs(nClusters) # correct the input for user
+  }
+  
+  if(sum(probability) != 1) {
+    stop("Input values for probability should sum to 1.")
+  }
+  
+  
 
+  # Start analysis
   # Using a multinomial distribution to generate cluster memberships
+  # Provide a message to user about the number of clusters
+  message("Generating data for ", nClusters, " clusters.")
   zValue <- t(stats::rmultinom(100, size = 1, prob = probability))
-
+  
   # Calculating the number of parameters
-  kParameters <- ((dimension + 1) * dimension) / 2 + dimension + (clusters - 1)
-
+  kParameters <- ((dimensionality + 1) * dimensionality) / 2 +
+    dimensionality + (nClusters - 1)
+  
   # Calculating BIC
   BIC <- - 2 * loglikelihood + (kParameters * log(observations))
-
+  
   # Calculating AIC
   AIC <- - 2 * loglikelihood + (2 * kParameters)
-
+  
   # Calculating ICL
-  mapz <- mclust::unmap(mclust::map(zValue[, 1:clusters]))
+  mapz <- mclust::unmap(mclust::map(zValue[, 1:nClusters]))
   forICL <- function(gClusters)
-    {sum(log(zValue[which(mapz[, gClusters] == 1), gClusters]))}
-  ICL <- (BIC + sum(sapply(1:clusters, forICL)))
-
+  {sum(log(zValue[which(mapz[, gClusters] == 1), gClusters]))}
+  ICL <- (BIC + sum(sapply(1:nClusters, forICL)))
+  
   Results <- list(BICresults = BIC,
                   AICresults = AIC,
                   ICLresults = ICL)
+  
   class(Results) <- "InfCriteriaCalculation"
   return(Results)
 }
+
 # [END]
-
-
-
