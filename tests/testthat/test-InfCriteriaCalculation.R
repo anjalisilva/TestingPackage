e00963293486661c576ef55eaf7aeabe5062188f
Anjali Silva <anjali.silva@uhnresearch.ca>
2020-10-26 21:32:16 -0400

    Update test-InfCriteriaCalculation.R

--- tests/testthat/test-InfCriteriaCalculation.R
+++ tests/testthat/test-InfCriteriaCalculation.R
@@ -4,44 +4,49 @@ library(TestingPackage)
 test_that("model fitting with three clusters", {
 
   loglikelihood = -1001.01
-  clusters = 3
-  dimension = 6
+  nClusters = 3
+  dimensionality = 6
   observations = 1000
   probability = c(0.1, 0.5, 0.4)
 
   InformationCriteria <- InfCriteriaCalculation(
-                               loglikelihood = loglikelihood,
-                               clusters = clusters,
-                               dimension = dimension,
-                               observations = observations,
-                               probability = probability)
+    loglikelihood = loglikelihood,
+    nClusters = nClusters,
+    dimensionality = dimensionality,
+    observations = observations,
+    probability = probability)
 
-  expect_that(InformationCriteria, is_a("InfCriteriaCalculation"))
-  expect_that(trunc(InformationCriteria$BICresults), equals(2202))
-  expect_that(trunc(InformationCriteria$AICresults), equals(2060))
-  expect_that(trunc(InformationCriteria$ICLresults), equals(2202))
+  expect_type(InformationCriteria, "list")
+  expect_s3_class(InformationCriteria, "InfCriteriaCalculation")
+  expect_length(InformationCriteria, 3)
+  expect_identical(trunc(InformationCriteria$BICresults), 2202)
+  expect_identical(trunc(InformationCriteria$AICresults), 2060)
+  expect_identical(trunc(InformationCriteria$ICLresults), 2202)
 })
 
 
 test_that("model fitting with two clusters", {
 
   loglikelihood = -2001.01
-  clusters = 2
-  dimension = 6
+  nClusters = 2
+  dimensionality = 6
   observations = 1000
   probability = c(0.4, 0.6)
 
   InformationCriteria <- InfCriteriaCalculation(
     loglikelihood = loglikelihood,
-    clusters = clusters,
-    dimension = dimension,
+    nClusters = nClusters,
+    dimensionality = dimensionality,
     observations = observations,
     probability = probability)
 
-  expect_that(InformationCriteria, is_a("InfCriteriaCalculation"))
-  expect_that(trunc(InformationCriteria$BICresults), equals(4195))
-  expect_that(trunc(InformationCriteria$AICresults), equals(4058))
-  expect_that(trunc(InformationCriteria$ICLresults), equals(4195))
+
+  expect_type(InformationCriteria, "list")
+  expect_s3_class(InformationCriteria, "InfCriteriaCalculation")
+  expect_length(InformationCriteria, 3)
+  expect_identical(trunc(InformationCriteria$BICresults), 4195)
+  expect_identical(trunc(InformationCriteria$AICresults), 4058)
+  expect_identical(trunc(InformationCriteria$ICLresults), 4195)
 })
 
-
+# [END]
