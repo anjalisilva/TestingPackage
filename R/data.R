b5f1ec7fd47aa8b5f30709c59ed737a06d49a36b
Anjali Silva <a.silva@utoronto.ca>
2021-10-31 16:55:52 -0400

    Documentation for raw data

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Gene Counts From An RNAseq Experiment
+#' Gene Counts From An RNAseq Experiment 1
 #'
 #' An RNAseq experiment conductd using bean plants from 2016 in Canada.
 #'
@@ -16,6 +16,25 @@
 #'  GeneCounts
 #' }
 "GeneCounts"
+
+#' Gene Counts Dataset From An RNAseq Experiment 2
+#'
+#' An RNAseq experiment conductd using bean plants from 2016 in Canada.
+#'
+#' @source University of Guelph, Canada.
+#'
+#' @format A matrix with columns:
+#' \describe{
+#'  \item{Condition.1}{First condition, under which least light was provided
+#'    to plants.}
+#'  \item{Condition.2}{Second condition, under which most light was provided to plants.}
+#'  \item{Condition.3}{Third condition, the control.}
+#' }
+#' @examples
+#' \dontrun{
+#'  GeneCounts2
+#' }
+"GeneCounts2"
 
 # [END]
 
